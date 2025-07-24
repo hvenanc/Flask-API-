d182df01aa640ac7faabe05ed194ceee699a511a
hvenanc <hvs.git@gmail.com>
2025-07-24 14:54:56 -0300

    (fix) - Correção da configuração do JWT no run.py

--- run.py
+++ run.py
@@ -6,6 +6,9 @@ from app import create_app
 load_dotenv()
 app = create_app()
 app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_API")
+app.config["JWT_TOKEN_LOCATION"] = ["headers"]  # ← ESSA É A QUE ESTÁ FALTANDO
+app.config["JWT_HEADER_NAME"] = "Authorization"
+app.config["JWT_HEADER_TYPE"] = "Bearer"
 
 jwt = JWTManager(app)
 
