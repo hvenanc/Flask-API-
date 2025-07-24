ddb188a69075e0a08a6b82abe89a4d4a88a9ea5f
hvenanc <hvs.git@gmail.com>
2025-07-24 18:18:00 -0300

    (fix) - Refatoração nos módulo app da aplicação

--- run.py
+++ run.py
@@ -1,16 +1,6 @@
-from flask_jwt_extended import JWTManager
-from dotenv import load_dotenv
-import os
 from app import create_app
 
-load_dotenv()
 app = create_app()
-app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_API")
-app.config["JWT_TOKEN_LOCATION"] = ["headers"]  # ← ESSA É A QUE ESTÁ FALTANDO
-app.config["JWT_HEADER_NAME"] = "Authorization"
-app.config["JWT_HEADER_TYPE"] = "Bearer"
-
-jwt = JWTManager(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
